1c155670216a212bade91d8382c9070a000ad768
Divyavarshini E <selvivarshini9@gmail.com>
2026-09-03 19:57:56 +0530

    Added variables example

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 19;
+        double marks = 85.5;
+        String name = "Divyavarshini";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Marks: " + marks);
+    }
+}
