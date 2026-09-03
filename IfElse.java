36be1acfceea068c5cd0818d3a73dcdcada3b5ff
Divyavarshini E <selvivarshini9@gmail.com>
2026-09-03 20:00:53 +0530

    Added if else example

---
+++ IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse {
+    public static void main(String[] args) {
+
+        int marks = 75;
+
+        if (marks >= 50) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
